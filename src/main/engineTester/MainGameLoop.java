ebd59e8d5f21dba0ff94e3d30115d47491da2287
1604066yolo <karthikraman2011@gmail.com>
2018-02-15 09:12:22 -0500

    Added Basic Renderer and VAO/VBO Loader

--- src/main/engineTester/MainGameLoop.java
+++ src/main/engineTester/MainGameLoop.java
@@ -3,19 +3,37 @@ package main.engineTester;
 import org.lwjgl.opengl.Display;
 
 import main.renderEngine.DisplayManager;
+import main.renderEngine.Loader;
+import main.renderEngine.RawModel;
+import main.renderEngine.Renderer;
 
 public class MainGameLoop {
-	
+
 	public static void main(String args[]) {
-		
+
 		DisplayManager.createDisplay();
+
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+
+		float[] vertices = { -0.5f, 0.5f, 0f, 
+				-0.5f, -0.5f, 0f, 
+				0.5f, -0.5f, 0f, 
+				0.5f, -0.5f, 0f, 
+				0.5f, 0.5f, 0f, 
+				-0.5f, 0.5f, 0f };
 		
-		while(!Display.isCloseRequested()) {
+		RawModel model = loader.loadToVAO(vertices);
+
+		while (!Display.isCloseRequested()) {
+			renderer.prepare();
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
-		
+
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
-		
+
 	}
 
 }
