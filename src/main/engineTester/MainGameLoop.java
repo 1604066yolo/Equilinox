1ba6c28a06df32de647bb3d1a02be9c8487fdc95
1604066yolo <karthikraman2011@gmail.com>
2018-02-15 21:54:21 -0500

    Added basic texturing capability

--- src/main/engineTester/MainGameLoop.java
+++ src/main/engineTester/MainGameLoop.java
@@ -2,11 +2,13 @@ package main.engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import main.models.RawModel;
+import main.models.TexturedModel;
 import main.renderEngine.DisplayManager;
 import main.renderEngine.Loader;
-import main.renderEngine.RawModel;
 import main.renderEngine.Renderer;
 import main.shaders.StaticShader;
+import main.textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -30,13 +32,21 @@ public class MainGameLoop {
 				0, 1, 3,
 				3, 1, 2
 		};
+		float[] textureCoords = {
+				0, 0,
+				0, 1,
+				1,1, 
+				1, 0
+		};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("images"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 
 		while (!Display.isCloseRequested()) {
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
---
+++ src/main/models/TexturedModel.java
@@ -0,0 +1,23 @@
+package main.models;
+
+import main.textures.ModelTexture;
+
+public class TexturedModel {
+	
+	private RawModel rawModel;
+	private ModelTexture texture;
+	
+	public TexturedModel(RawModel model, ModelTexture texture) {
+		this.texture = texture;
+		this.rawModel = model;
+	}
+
+	public RawModel getRawModel() {
+		return rawModel;
+	}
+
+	public ModelTexture getTexture() {
+		return texture;
+	}
+
+}
